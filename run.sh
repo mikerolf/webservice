724cf70025682ce7972c248e960f3434edbc13ed
Mike Rolf <mikerolfuk@gmail.com>
2016-04-06 14:59:14 +0100

    Added build script.

---
+++ run.sh
@@ -0,0 +1,4 @@
+rm -fr out
+mkdir -p out/production/untitled
+javac -d out/production/untitled src/Program.java
+java -cp out/production/untitled Program
--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,4 @@
-public class Project {
-
+public class Program {
     public static void main(String[] args) {
         System.out.println("run");
     }
