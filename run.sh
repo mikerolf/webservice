da779d1a488ea7a70497245d916b16ca2c6ed06d
Mike Rolf <mikerolfuk@gmail.com>
2016-04-06 16:48:38 +0100

    Added simple web server.

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 rm -fr out
 mkdir -p out/production/untitled
 javac -d out/production/untitled src/Program.java
+
+echo "Server running..."
 java -cp out/production/untitled Program
+
+
+
+
--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,25 @@
+import com.sun.net.httpserver.*;
+
+import java.io.*;
+import java.net.*;
+
 public class Program {
-    public static void main(String[] args) {
-        System.out.println("run");
+
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/test", new MyHandler());
+        server.setExecutor(null);
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "Running.";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
     }
 }
